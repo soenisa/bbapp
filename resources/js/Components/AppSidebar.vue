ba973500aeb7bb1442ff7279b1f51219eaacb61d
Senisa <senisa@blackcart.co>
2022-03-26 20:56:29 -0400

    I got the nav working :''''''')
    * Needed to specify the outer div is flex-row ugh

---
+++ resources/js/Components/AppSidebar.vue
@@ -0,0 +1,47 @@
+<template>
+  <CSidebar visible>
+  <CSidebarBrand>Sidebar Brand</CSidebarBrand>
+  <CSidebarNav>
+    <li class="nav-title">Nav Title</li>
+    <CNavItem href="#">
+      <CIcon  customClassName="nav-icon" icon="cil-speedometer"/>
+      Nav item
+    </CNavItem>
+    <CNavItem href="#">
+        <CIcon  customClassName="nav-icon" icon="cil-speedometer"/>
+        With badge
+        <CBadge color="primary ms-auto">NEW</CBadge>
+    </CNavItem>
+    <CNavGroup>
+      <template #togglerContent>
+        <CIcon  customClassName="nav-icon" icon="cil-puzzle"/> Nav dropdown
+      </template>
+      <CNavItem href="#">
+        <CIcon  customClassName="nav-icon" icon="cil-puzzle"/> Nav dropdown item
+      </CNavItem>
+      <CNavItem href="#">
+         <CIcon  customClassName="nav-icon" icon="cil-puzzle"/> Nav dropdown item
+      </CNavItem>
+    </CNavGroup>
+  </CSidebarNav>
+  <CSidebarToggler/>
+</CSidebar>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+import { CSidebar, CSidebarBrand, CSidebarNav, CNavItem, CIcon, CBadge, CNavGroup, CSidebarToggler } from '@coreui/vue';
+export default {
+  name: 'AppSidebar',
+  components: {
+  },
+  setup() {
+    const store = useStore()
+    return {
+      sidebarUnfoldable: computed(() => store.state.sidebarUnfoldable),
+      sidebarVisible: computed(() => store.state.sidebarVisible),
+    }
+  },
+}
+</script>
--- resources/js/Layouts/Authenticated.vue
+++ resources/js/Layouts/Authenticated.vue
@@ -1,20 +1,21 @@
 <script>
-import { CContainer, CSidebar, CHeader, CFooter } from '@coreui/vue'
+import { CContainer, CHeader, CFooter } from '@coreui/vue';
+import AppSidebar from '@/components/AppSidebar.vue';
 
 export default {
   name: 'DefaultLayout',
   components: {
+    AppSidebar,
     CFooter,
     CHeader,
-    CSidebar,
     CContainer,
   },
 }
 </script>
 
 <template>
-  <div>
-    <CSidebar />
+  <div class="d-flex">
+    <AppSidebar />
     <div class="wrapper d-flex flex-column min-vh-100 bg-light">
       <CHeader />
       <div class="body flex-grow-1 px-3">
